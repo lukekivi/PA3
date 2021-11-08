7ab458396049479ae0436203b9b80187114134f7
Lucas Kivi <oscar@Friend.localdomain>
2021-11-08 12:56:25 -0600

    Commented main and fixed some logic.

--- src/main.c
+++ src/main.c
@@ -11,16 +11,18 @@ void writeBalanceToFiles(void) {
 }
 
 int main(int argc, char *argv[]){
-    int nConsumers = 0;
-    char* path = NULL;
-    int mode = 0;
-    int queueBufferSize = -1;
+    int nConsumers = 0;            // Number of consumer threads to be created.
+    char* path = NULL;             // Path of input file
+    int mode = 0;                  // App mode that specifies use of bounded buffer or log output
+    int queueBufferSize = -1;      // initialized with an invalid value
 
     // Argument check
-    if (argc < 2) {
+    if (argc < 3) {
         fprintf(stderr, "ERROR: %d is an insufficient amount of arguments.\n", argc);
         exit(EXIT_FAILURE);
     } else {
+        
+        // First argument should be an the number of consumer threads the user desires to create
         if (isDigits(argv[1]) == -1) {
             fprintf(stderr, "ERROR: first argument \"%s\" should be an integer.\n",argv[1]);
             exit(EXIT_FAILURE);
@@ -28,27 +30,41 @@ int main(int argc, char *argv[]){
             nConsumers = atoi(argv[1]);
         }
 
+        // Second argument is the path for the input file
         path = argv[2];
 
-        if (argc == 5) {
+        // If there are more than 3 arguments there is a mode specification 
+        if (argc > 3) {
             if (argv[3][0] == '-') {
-                
+                // check the mode
                 if ((mode = parseModeArg(argv[3])) == -1) {
                     fprintf(stderr, "ERROR: %s is an invalid argument.\n", argv[3]);
                     exit(EXIT_FAILURE);
                 }
 
-                if (mode == 2 || mode == 3) {
-                    if(isDigits(argv[4]) == -1) {
-                        fprintf(stderr, "ERROR: a bufferSize argument must be provided immediately after \"%s\".\n",argv[3]);
+                // if bounded buffer was requested get the buffer size
+                if ((mode == 2 || mode == 3)) {
+                    if (argc != 5 ||isDigits(argv[4]) == -1) {
+                        fprintf(stderr, "ERROR: an integer bufferSize argument must be provided immediately after \"%s\".\n",argv[3]);
                         exit(EXIT_FAILURE);
                     } else {
                         queueBufferSize = atoi(argv[4]);
                     }
                 }
+            } else {
+                fprintf(stderr, "ERROR: arguments should be in this order \"number of consumers, input file path, mode, (if '-b') buffer size\".\n");
+                exit(EXIT_FAILURE);
             }
         }
     }
+
+    /**
+     * Modes:
+     * - 0: bare minimum functionality. No mode specified.
+     * - 1: generate log output.
+     * - 2: use a bounded buffer.
+     * - 3: generate log output and use a bounded buffer.
+     */
 
     bookeepingCode();
     
