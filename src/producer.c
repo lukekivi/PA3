16528f817aa63923d234307eb2f21de9d5438faa
DallasSchauer <60755082+DallasSchauer@users.noreply.github.com>
2021-11-10 19:09:16 -0600

    Update Producer
    Added line count, semaphores, declares global q.

--- src/producer.c
+++ src/producer.c
@@ -1,5 +1,8 @@
 #include "producer.h"
 
+struct Queue* q;
+sem_t mutex;
+sem_t staged;
 /**
  *
  * Producer thread will read from the file and write data to
@@ -7,7 +10,7 @@
  */
 void *producer(void *arg){
 
-    printf("producer\n");
+    printf("In the producer.\n");
 
     //TODO: open the file and read its content line by line
 
@@ -15,22 +18,40 @@ void *producer(void *arg){
     ssize_t nread;
     char buffer[chunkSize]; // chunk size for now
     size_t len = chunkSize;
+    int lineCount = 0;
 
     // read until EOF
     while (nread = getLineFromFile(fd_in, buffer, len) != -1) {
+      lineCount++;
       //Send data to the shared queue
       struct Packet* p = (struct Packet*) malloc(sizeof(struct Packet));
       p->line = buffer;
+      p->lineCount = lineCount;
+
+      // Testing to see that the line and packet are the same.
+      printf("Line: %s", buffer);
+      printf("Packet: %s", p->line);
 
       struct Node* n = (struct Node*)malloc(sizeof(struct Node));
       n->next = NULL;
       n->packet = p;
 
-      //enqueue(q, n);  q is not recognized at the moment for some reason.
-      freeNode(n);
+      // Testing to see that node succesfully copied.
+      printf("Node: %s", n->packet->line);
+
+      sem_wait(&mutex);
+      enqueue(q, n);
+      sem_post(&mutex);
+      sem_post(&staged);
+      free(n);
+      free(p);
     }
+
     //When reaching the end of the file, send EOF message
     printf("Reached EOF.\n");
+    printf("Printing queue:\n");
+    // Print queue for testing.
+    printQueue(q);
 
     // cleanup and exit
     return NULL;
